122315625f45413794cb85252fb24e0e03a15990
Vinod J <Vinod J@Vinod>
2018-08-27 16:24:13 +0530

    Commit2

--- TestGroupsMav/src/test/java/testgroup/TestScrollDownJS.java
+++ TestGroupsMav/src/test/java/testgroup/TestScrollDownJS.java
@@ -15,7 +15,7 @@ public void ScrollDown()
 	WebDriver driver=new ChromeDriver();
 	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	driver.manage().window().maximize();
-	driver.get("file:///C:/Users/Vinod%20J/Desktop/Test6.html");
+	driver.get("file:///C:/Users/Vinod%20J/Desktop/Selenium/HTML%20Files/Test6.html");
 	RemoteWebDriver r =(RemoteWebDriver)driver;
 	String c="window.scrollTo(0,document.body.scrollHeight)"; //window.scrollTo(x,y)
 	r.executeScript(c);
--- TestGroupsMav/src/test/java/testgroup/TestScrollDownToMiddleJS.java
+++ TestGroupsMav/src/test/java/testgroup/TestScrollDownToMiddleJS.java
@@ -19,7 +19,7 @@ public class TestScrollDownToMiddleJS {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("file:///C:/Users/Vinod%20J/Desktop/Test6.html");
+		driver.get("file:///C:/Users/Vinod%20J/Desktop/Selenium/HTML%20Files/Test6.html");
 		RemoteWebDriver r =(RemoteWebDriver)driver;
 		WebElement t1= driver.findElement(By.id("t1"));
 		 Point p= t1.getLocation();
